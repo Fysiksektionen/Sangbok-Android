94375ad18b6283abaf7fd9472e66027e8c29b2a4
Emil Ringh <emil.ringh@gmail.com>
2014-12-11 21:31:05 +0100

    Version 1.1 zoom och skärm igång
    Nu kan man Zooma med volymknapparna när man tittar på en sång och
    skärmen slocknar inte när man läser en sång (setting, går att stänga av)

--- src/org/fysiksektionen/sangbok/DisplaySang.java
+++ src/org/fysiksektionen/sangbok/DisplaySang.java
@@ -3,13 +3,17 @@ package org.fysiksektionen.sangbok;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.TypedValue;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.text.method.ScrollingMovementMethod;
 import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Build;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 public class DisplaySang extends Activity {
 
@@ -17,8 +21,17 @@ public class DisplaySang extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_display_sang);
-//		// Show the Up button in the action bar.
+		// Show the Up button in the action bar.
 		setupActionBar();
+		
+		// Determines if the screen is always on or not
+		SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+		if (sharedPrefs.getBoolean("screen_on", true) ) {
+			getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		}
+		else {
+			getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		}
 		
 		//Get the intent to get the Sang
 		Intent intent = getIntent();
@@ -83,6 +96,28 @@ public class DisplaySang extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	@Override
+	public boolean dispatchKeyEvent(KeyEvent event) {
+	    int action = event.getAction();
+	    int keyCode = event.getKeyCode();
+	    switch (keyCode) {
+	        case KeyEvent.KEYCODE_VOLUME_UP:
+	            if (action == KeyEvent.ACTION_DOWN) {
+	            	TextView textView = (TextView) findViewById(R.id.show_sang_text);
+	    		    textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, textView.getTextSize()*1.1F);
+	            }
+	            return true;
+	        case KeyEvent.KEYCODE_VOLUME_DOWN:
+	            if (action == KeyEvent.ACTION_DOWN) {
+	            	TextView textView = (TextView) findViewById(R.id.show_sang_text);
+	    		    textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, textView.getTextSize()*0.9090909F);
+	            }
+	            return true;
+	        default:
+	            return super.dispatchKeyEvent(event);
+	    }
 	}
 
 }
